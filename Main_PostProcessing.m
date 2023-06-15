151e87bec7129ad26580dd32e6cc0e0642d5baab
SSL-Grad <SSL-Grad@DESKTOP-I1NCEOB>
2023-06-15 15:13:34 -0700

    Edited comment in main

--- Main_PostProcessing.m
+++ Main_PostProcessing.m
@@ -4,8 +4,7 @@
 % Date: 3/17/2021
 
 % Script to load in saved matfiles, generate figures, and perform
-% statistical analysis on the data - No statistical analysis as I don't
-% have a large enough data set
+% statistical analysis on the data 
 
 
 %% Close clear workspace/command window
